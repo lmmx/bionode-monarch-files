c39228fda4f3e219579f99ff31acec460e07f8f7
Louis <louismmx@gmail.com>
2016-07-09 15:57:12 +0100

    Sorted table

---
+++ README.md
@@ -0,0 +1,3 @@
+# bionode-monarch-files
+
+Accessory files to [bionode-hack/bionode-monarch](https://github.com/bionode-hack/bionode-monarch)
--- data/tables-to-accessions.sh
+++ data/tables-to-accessions.sh
@@ -4,4 +4,4 @@ echo "$(
 	cut -d $'\t' -f 5 align_doid_efo.tbl 
 	cut -d $'\t' -f 1 omim-id-name.tbl 
 )" | \
-	sort 
+	sort > disease_accessions.txt
